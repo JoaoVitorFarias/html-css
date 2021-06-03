3f64a9c110635f03cfb8e953546022f0d3d8f3de
JoaoVitorFarias <jvlopesfarias@gmail.com>
2021-06-03 16:07:13 -0300

    Add scrolling effects

--- OmniFood/resources/js/script.js
+++ OmniFood/resources/js/script.js
@@ -13,4 +13,46 @@ $(document).ready(function(){
         offset: '60px;'
     });
 
+    $('.js--scroll-to-plans').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 900);
+    });
+
+    $('.js--scroll-to-start').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 500);
+    });
+
+    $(function(){
+        $('a[href*=#]:not([href=#])').click(function(){
+            if (location.pathname.replace(/^\//,'') && location.hostname == this.hostname)
+            {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length)
+                {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animate fadeIn');
+    }, {
+        offset: '50%;'
+    });
+
+    $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animate fadeIn');
+    }, {
+        offset: '50%;'
+    });
+
+    $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animate fadeInUp');
+    }, {
+        offset: '50%;'
+    });
 });
